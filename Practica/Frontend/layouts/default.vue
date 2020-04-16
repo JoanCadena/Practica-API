509b7065c1265769ab78c1086582fc72b0c7e901
JoanCadena <inmortal_20@live.com>
2020-04-16 15:47:18 -0500

    primera incorporación con el frontend

---
+++ Practica/Frontend/layouts/default.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="dark">
+        <img
+          src="https://cdn.iconscout.com/icon/premium/png-256-thumb/bookmark-268-468642.png"
+          fluid
+          alt="Responsive image"
+          height="50px"
+          width="50px"
+        />
+        <b-navbar-brand href="#"></b-navbar-brand>
+        <b-navbar-brand href>API - BookMarks</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item :to="{name:'marcadores'}">Ver Marcadores</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Buscar"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Buscar</b-button>
+            </b-nav-form>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+
+    <nuxt />
+  </div>
+</template>
+
+<style>
+.navbar.navbar-dark.bg-dark {
+  background-color: blue !important;
+}
+
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ Practica/Frontend/pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <div>
+      <b-container fluid="md">
+        <center>
+          <h1>Marcadores - BookMarks</h1>
+        </center>
+        <br />
+        <div class="info-columna">
+          <p>
+            Un marcador de Internet es la localización almacenada de una página web de forma que puede
+            ser revisitada más adelante. La localización de una página web suele expresarse con una URL.
+            Todos los navegadores web modernos incorporan como característica la posibilidad de catalogar
+            y acceder fácilmente a las webs que el usuario ya ha visitado y guardado. Los enlaces
+            se suelen denominar favoritos en Internet Explorer y Safari; y marcadores en Mozilla Firefox
+            y Google Chrome, los cuatro navegadores web más utilizados en el mercado.
+            <br />Además de los navegadores, existen actualmente muchas herramientas externas cuyo propósito
+            principal es el de gestionar estos marcadores.
+            <br />
+          </p>
+          <hr />
+          <br />
+          <img src="../static/1.png" class="d-block w-100" alt="..." />
+          <br />
+          <br />
+        </div>
+      </b-container>
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
